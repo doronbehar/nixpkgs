e35e086f08a58169e801e13c4aacee7a99c2514c
Doron Behar <doron.behar@gmail.com>
2019-10-05 10:17:45 +0300

    Add platforms to meta

--- pkgs/development/libraries/essentia/default.nix
+++ pkgs/development/libraries/essentia/default.nix
@@ -80,6 +80,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/MTG/essentia";
     description = "C++ library with python bindings which implements similarity measures and classiﬁcations on the results of audio analysis, and generates classiﬁcation models that Essentia can use to compute high-level description of music";
     maintainers = with maintainers; [ doronbehar ];
+    platforms = platforms.all;
     license = licenses.agpl3;
   };
 }
