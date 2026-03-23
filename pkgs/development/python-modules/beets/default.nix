97f55d2f0dd4e79aa994c67d2b733ede06a5009d
Doron Behar <doron.behar@gmail.com>
2026-03-23 14:58:10 +0200

    python3.pkgs.beets: Revert "remove sphinx man pages"
    This reverts commit 68f1afdb4d8a88a29ae113eacff48b78fdb7913d.

--- pkgs/development/python-modules/beets/default.nix
+++ pkgs/development/python-modules/beets/default.nix
@@ -52,6 +52,11 @@
 
   # native
   gobject-introspection,
+  sphinxHook,
+  sphinx-design,
+  sphinx-copybutton,
+  sphinx-toolbox,
+  pydata-sphinx-theme,
 
   # buildInputs
   gst_all_1,
@@ -156,6 +161,11 @@ buildPythonPackage (finalAttrs: {
 
   nativeBuildInputs = [
     gobject-introspection
+    sphinxHook
+    sphinx-design
+    sphinx-copybutton
+    sphinx-toolbox
+    pydata-sphinx-theme
   ]
   ++ extraNativeBuildInputs;
 
@@ -167,7 +177,19 @@ buildPythonPackage (finalAttrs: {
 
   outputs = [
     "out"
+    "doc"
+    "man"
   ];
+  sphinxBuilders = [
+    "html"
+    "man"
+  ];
+  # Causes an installManPage error. Not clear why this directory gets generated
+  # with the manpages. The same directory is observed correctly in
+  # $doc/share/doc/beets-${version}/html
+  preInstallSphinx = ''
+    rm -r .sphinx/man/man/_sphinx_design_static
+  '';
 
   postInstall = ''
     mkdir -p $out/share/zsh/site-functions
